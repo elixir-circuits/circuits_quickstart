6c1154025b2ac6918048d3f8c30865f7f99d2dfb
Frank Hunleth <fhunleth@troodon-software.com>
2019-07-12 09:12:10 -0400

    Start inets for demo convenience

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule CircuitsQuickstart.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :inets]
     ]
   end
 
