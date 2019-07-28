fb978bd32b1c8ee7f1e279b4e32136ac53c111b3
Frank Hunleth <fhunleth@troodon-software.com>
2019-07-27 22:32:41 -0400

    Add rpi4 and update systems

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule CircuitsQuickstart.MixProject do
   use Mix.Project
 
-  @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :bbb, :x86_64]
+  @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :rpi4, :bbb, :x86_64]
   @app :circuits_quickstart
 
   def project do
@@ -68,6 +68,7 @@ defmodule CircuitsQuickstart.MixProject do
       {:nerves_system_rpi2, "~> 1.8", runtime: false, targets: :rpi2},
       {:nerves_system_rpi3, "~> 1.8", runtime: false, targets: :rpi3},
       {:nerves_system_rpi3a, "~> 1.8", runtime: false, targets: :rpi3a},
+      {:nerves_system_rpi4, "~> 1.8", runtime: false, targets: :rpi4},
       {:nerves_system_bbb, "~> 2.3", runtime: false, targets: :bbb},
       {:nerves_system_x86_64, "~> 1.8", runtime: false, targets: :x86_64}
     ]
