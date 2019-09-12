305ec79baffeb3b6721d1be22f11e39fd287eee9
Frank Hunleth <fhunleth@troodon-software.com>
2019-09-12 10:20:33 -0400

    Don't strip dev builds
    Stripping builds removes the docs which is not helpful to anyone using
    this project.

--- mix.exs
+++ mix.exs
@@ -37,7 +37,8 @@ defmodule CircuitsQuickstart.MixProject do
       overwrite: true,
       cookie: "#{@app}_cookie",
       include_erts: &Nerves.Release.erts/0,
-      steps: [&Nerves.Release.init/1, :assemble]
+      steps: [&Nerves.Release.init/1, :assemble],
+      strip_beams: Mix.env() == :prod
     ]
   end
 
