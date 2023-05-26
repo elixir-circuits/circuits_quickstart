d574974f2e910ea0274b62c833b42165167b16fb
Frank Hunleth <fhunleth@troodon-software.com>
2023-05-26 08:33:32 -0400

    Bump NervesMOTD and move logo to config
    This allows you to type `NervesMOTD.print` any time and get the same
    printout that you get when logging in.

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,40 @@ config :nerves, :firmware,
 
 config :nerves, source_date_epoch: "1603310828"
 
+config :nerves_motd,
+  logo: [
+    IO.ANSI.color(5),
+    """
+
+                    ;kX'
+                  ,0XXXl
+                 xNXNNXX.
+               'KNNXXXXX0.
+              ;XNNNNNNNNN0.
+             ;XNNNNNNNNNNNX:
+            .XNNNXXXXXXXXXXXO.
+            kNNNXNNNNXXNXXNNXNo
+           .NNNXNNNNNXXNNXNNXXXO
+           cXXXNNNNNNXXNNNNNNNNNd
+           lNNNNNNNNNXXNXXXNNNNNK
+           'NNXNXNXXXXXXXXXNNNNNk
+            oNXNXXXXXNXXXNNNNXXX.
+             :KXXXXXXNXXXNNNNXk.
+               ;xXNXXXXNXXX0o.
+                  .',::;,.
+
+    """,
+    IO.ANSI.reset(),
+    """
+          Elixir Circuits Quickstart
+
+    All of the Elixir Circuits projects are available in this firmware
+    image. See https://github.com/elixir-circuits/circuits_quickstart for
+    more details.
+
+    """
+  ]
+
 if Mix.target() != :host do
   import_config "target.exs"
 end
