fec88fda9b1fb302d910cf7e7b639968a99958e7
Frank Hunleth <fhunleth@troodon-software.com>
2023-10-09 21:56:13 -0400

    Bump Nerves systems to 1.24.1 versions (OTP 26.1.1)

--- mix.exs
+++ mix.exs
@@ -60,15 +60,15 @@ defmodule CircuitsQuickstart.MixProject do
       {:nerves_pack, "~> 0.7.0", targets: @all_targets},
 
       # Dependencies for specific targets
-      {:nerves_system_rpi, "~> 1.23", runtime: false, targets: :rpi},
-      {:nerves_system_rpi0, "~> 1.23", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.23", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.23", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.23", runtime: false, targets: :rpi3a},
-      {:nerves_system_rpi4, "~> 1.23", runtime: false, targets: :rpi4},
+      {:nerves_system_rpi, "~> 1.24", runtime: false, targets: :rpi},
+      {:nerves_system_rpi0, "~> 1.24", runtime: false, targets: :rpi0},
+      {:nerves_system_rpi2, "~> 1.24", runtime: false, targets: :rpi2},
+      {:nerves_system_rpi3, "~> 1.24", runtime: false, targets: :rpi3},
+      {:nerves_system_rpi3a, "~> 1.24", runtime: false, targets: :rpi3a},
+      {:nerves_system_rpi4, "~> 1.24", runtime: false, targets: :rpi4},
       {:nerves_system_bbb, "~> 2.18", runtime: false, targets: :bbb},
       {:nerves_system_osd32mp1, "~> 0.14", runtime: false, targets: :osd32mp1},
-      {:nerves_system_x86_64, "~> 1.23", runtime: false, targets: :x86_64},
+      {:nerves_system_x86_64, "~> 1.24", runtime: false, targets: :x86_64},
       {:nerves_system_npi_imx6ull, "~> 0.11", runtime: false, targets: :npi_imx6ull},
       {:nerves_system_grisp2, "~> 0.7", runtime: false, targets: :grisp2},
       {:nerves_system_mangopi_mq_pro, "~> 0.5", runtime: false, targets: :mangopi_mq_pro}
