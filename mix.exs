4ab9c034e2ee1fd0bfb14a28cea78db89344fc1d
Frank Hunleth <fhunleth@troodon-software.com>
2024-02-18 18:15:20 -0500

    Bump systems to nerves_system_br 1.26.1 versions

--- mix.exs
+++ mix.exs
@@ -24,8 +24,8 @@ defmodule CircuitsQuickstart.MixProject do
     [
       app: @app,
       version: @version,
-      elixir: "~> 1.12",
-      archives: [nerves_bootstrap: "~> 1.9"],
+      elixir: "~> 1.16",
+      archives: [nerves_bootstrap: "~> 1.10"],
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       releases: [{@app, release()}],
@@ -62,20 +62,20 @@ defmodule CircuitsQuickstart.MixProject do
       {:nerves_pack, "~> 0.7.0", targets: @all_targets},
 
       # Dependencies for specific targets
-      {:nerves_system_rpi, "~> 1.24", runtime: false, targets: :rpi},
-      {:nerves_system_rpi0, "~> 1.24", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi0_2, "~> 1.24", runtime: false, targets: :rpi0_2},
-      {:nerves_system_rpi2, "~> 1.24", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.24", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.24", runtime: false, targets: :rpi3a},
-      {:nerves_system_rpi4, "~> 1.24", runtime: false, targets: :rpi4},
-      {:nerves_system_rpi5, "~> 0.0.1", runtime: false, targets: :rpi5},
-      {:nerves_system_bbb, "~> 2.18", runtime: false, targets: :bbb},
-      {:nerves_system_osd32mp1, "~> 0.14", runtime: false, targets: :osd32mp1},
-      {:nerves_system_x86_64, "~> 1.24", runtime: false, targets: :x86_64},
-      {:nerves_system_npi_imx6ull, "~> 0.11", runtime: false, targets: :npi_imx6ull},
-      {:nerves_system_grisp2, "~> 0.7", runtime: false, targets: :grisp2},
-      {:nerves_system_mangopi_mq_pro, "~> 0.5", runtime: false, targets: :mangopi_mq_pro}
+      {:nerves_system_rpi, "~> 1.26", runtime: false, targets: :rpi},
+      {:nerves_system_rpi0, "~> 1.26", runtime: false, targets: :rpi0},
+      {:nerves_system_rpi0_2, "~> 1.26", runtime: false, targets: :rpi0_2},
+      {:nerves_system_rpi2, "~> 1.26", runtime: false, targets: :rpi2},
+      {:nerves_system_rpi3, "~> 1.26", runtime: false, targets: :rpi3},
+      {:nerves_system_rpi3a, "~> 1.26", runtime: false, targets: :rpi3a},
+      {:nerves_system_rpi4, "~> 1.26", runtime: false, targets: :rpi4},
+      {:nerves_system_rpi5, "~> 0.1", runtime: false, targets: :rpi5},
+      {:nerves_system_bbb, "~> 2.21", runtime: false, targets: :bbb},
+      {:nerves_system_osd32mp1, "~> 0.17", runtime: false, targets: :osd32mp1},
+      {:nerves_system_x86_64, "~> 1.26", runtime: false, targets: :x86_64},
+      {:nerves_system_npi_imx6ull, "~> 0.14", runtime: false, targets: :npi_imx6ull},
+      {:nerves_system_grisp2, "~> 0.10", runtime: false, targets: :grisp2},
+      {:nerves_system_mangopi_mq_pro, "~> 0.8", runtime: false, targets: :mangopi_mq_pro}
     ]
   end
 
